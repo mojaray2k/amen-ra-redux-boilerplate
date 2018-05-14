e0fcb21a609e98178a743632783d7aff35d65f5b
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-05-14 14:41:50 -0400

    Adding the state and get state steps

---
+++ index.js
@@ -0,0 +1,15 @@
+function createStore () {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state. (getState)
+  // 3. Listen to changes on the state. (subscribe)
+  // 4. Update the state (dispatch)
+
+  let state
+
+  const getState = () => state
+
+  return {
+    getState
+  }
+}
